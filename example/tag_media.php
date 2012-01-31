1bdfec4294718ea5b6e28844857d3795ec19a764
Christian Metz <christian-metz1@gmx.net>
2012-01-31 17:43:19 +0100

    Example for the new Tag Endpoints.

---
+++ example/tag_media.php
@@ -0,0 +1,18 @@
+<?php
+
+require 'instagram.class.php';
+
+// Initialize class for public requests
+$instagram = new Instagram('YOUR_APP_KEY');
+
+$tag = 'winter';
+
+// Get recently tagged media
+$media = $instagram->getTagMedia($tag);
+
+// Display results
+foreach ($media->data as $data) {
+  echo "<img src=\"{$data->images->thumbnail->url}\">";
+}
+
+?>
