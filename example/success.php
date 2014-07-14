f1c0ef1fe0c7b19d675f4541ddb9f9364b642f52
Marcelo Goebel Machado <mgoebelm@gmail.com>
2014-07-14 06:46:05 -0300

    FIX examples media hover selector

--- example/success.php
+++ example/success.php
@@ -116,7 +116,6 @@ if (isset($code)) {
             $media.stop().animate({ marginTop: -(height - 82) }, 1000);
           }, function() {
             var $media = $(this).find('.media');
-            var height = $media.height();
             $media.stop().animate({ marginTop: '0px' }, 1000);
           }
         );
