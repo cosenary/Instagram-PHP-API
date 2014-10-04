c9b5949120ba38f8003c563f4ba67674edc03af4
Christian Metz <christian-metz1@gmx.net>
2014-10-04 19:39:03 +0200

    Avoid PHP warning due to empty captions

--- example/popular.php
+++ example/popular.php
@@ -48,7 +48,7 @@ $result = $instagram->getPopularMedia();
             // create meta section
             $avatar = $media->user->profile_picture;
             $username = $media->user->username;
-            $if (!empty($media->caption->text)) $comment = $media->caption->text; else $comment ="";
+            $comment = (!empty($media->caption->text)) ? $media->caption->text : '';
             $content .= "<div class=\"content\">
                            <div class=\"avatar\" style=\"background-image: url({$avatar})\"></div>
                            <p>{$username}</p>
