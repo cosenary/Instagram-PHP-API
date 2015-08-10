9efb1912e588b1ea831e235cfdb0e32d94626dd2
devisualy <devisualy@gmail.com>
2015-08-10 20:14:11 +0900

    Fix array phpdoc.

--- src/Instagram.php
+++ src/Instagram.php
@@ -69,14 +69,14 @@ class Instagram
     /**
      * Available scopes.
      *
-     * @var array
+     * @var string[]
      */
     private $_scopes = array('basic', 'likes', 'comments', 'relationships');
 
     /**
      * Available actions.
      *
-     * @var array
+     * @var string[]
      */
     private $_actions = array('follow', 'unfollow', 'block', 'unblock', 'approve', 'deny');
 
