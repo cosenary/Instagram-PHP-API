fe658aeac8bd1d3c222d9514f9c329355a8ee194
Gilgledson souza <gilgledson96@gmail.com>
2016-06-21 23:38:04 -0300

    function for get image id
    the function is used to convert the image url In id To Be Used in Function of like

--- src/Instagram.php
+++ src/Instagram.php
@@ -149,7 +149,21 @@ class Instagram
 
         return $this->_makeCall('users/search', false, $params);
     }
-
+    /**
+     * Generates the OAuth login URL.
+     *
+     * @param string url image
+     *
+     * @return string id image
+     *
+     * @throws \MetzWeb\Instagram\InstagramException
+     */
+     public function getImageID($url){
+       $json_file = file_get_contents("http://api.instagram.com/oembed?url=".$url);
+       $json_str = json_decode($json_file, true);         
+       return $json_str['media_id'];
+         
+     }
     /**
      * Get user info.
      *
