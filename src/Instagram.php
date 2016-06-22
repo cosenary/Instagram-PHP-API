7c310d5169e342fd10ff62f1842bb10b70b3244b
Gilgledson souza <gilgledson96@gmail.com>
2016-06-21 23:53:55 -0300

    Update Instagram.php

--- src/Instagram.php
+++ src/Instagram.php
@@ -158,10 +158,11 @@ class Instagram
      *
      * @throws \MetzWeb\Instagram\InstagramException
      */
-      public function getImageID($url)
+      public function getImageid($url)
 +     {
 +         $json_file = file_get_contents("http://api.instagram.com/oembed?url=".$url);
 +         $json_str = json_decode($json_file, true);
+
 +         return $json_str['media_id'];
       }
     /**
