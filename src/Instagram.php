487c747a2f9cb403c69d3a77c04c5cea32cc1562
Gilgledson souza <gilgledson96@gmail.com>
2016-06-22 00:04:05 -0300

    Update Instagram.php

--- src/Instagram.php
+++ src/Instagram.php
@@ -161,7 +161,7 @@ class Instagram
      */
       public function getImageid($url)
       {
-           $json_file = file_get_contents("http://api.instagram.com/oembed?url=".$url);
+           $json_file = file_get_contents('http://api.instagram.com/oembed?url='.$url);
            $json_str  = json_decode($json_file, true);
     
            return $json_str['media_id'];
