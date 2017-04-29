feecbaef81c87f50bd43c11eefa01dfa235ad493
Iain Mullan <iain.mullan@gmail.com>
2017-04-29 18:26:43 +0100

    fix the name of ratelimit header returned from API
    x-ratelimit-remaining instead of X-Ratelimit-Remaining

--- src/Instagram.php
+++ src/Instagram.php
@@ -630,7 +630,7 @@ class Instagram
         $headers = $this->processHeaders($headerContent);
 
         // get the 'X-Ratelimit-Remaining' header value
-        $this->_xRateLimitRemaining = $headers['X-Ratelimit-Remaining'];
+        $this->_xRateLimitRemaining = $headers['x-ratelimit-remaining'];
 
         if (!$jsonData) {
             throw new InstagramException('Error: _makeCall() - cURL error: ' . curl_error($ch));
